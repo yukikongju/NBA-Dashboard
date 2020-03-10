03ac7eded4afe64716354038640699ff727afeae
Emulie Chhor <emulie.chhor@hotmail.com>
2020-03-10 18:00:40 -0400

    Converted d_season_combined to numeric values

--- nbadashboard/stats_loader.R
+++ nbadashboard/stats_loader.R
@@ -1,5 +1,6 @@
 
 
+
 #---------------------------- Dependencies -------------------------------------------
 
 library(rvest)
@@ -8,11 +9,18 @@ library(plyr)
 library(parallel)
 library(janitor)
 library(jsonlite)
+library(gapminder)
+library(stringr)
+
+# ------------------------------------ Useful Links -------------------------------
+# updateSelectInput:
+# https://stackoverflow.com/questions/21465411/r-shiny-passing-reactive-to-selectinput-choices
+
 
 # ------------------------- remove conflict ------------------------------------------
 
 conflict_prefer("rename", "dplyr")
-
+conflict_prefer("mutate", "dplyr")
 
 # ----------------------------- Player stats by season -----------------------------------
 
@@ -39,8 +47,8 @@ getStats <-  function(url, season) {
     select(-c(Rk)) %>%
     distinct(Player, .keep_all = TRUE) %>%
     filter(!Player == "Player") %>%
-    mutate_all(na_if, "") %>% 
-    rename(Team=Tm)
+    mutate_all(na_if, "") %>%
+    rename(Team = Tm)
   
   iteration = parent.frame()$i[]
   
@@ -50,14 +58,21 @@ getStats <-  function(url, season) {
     mutate(Season = as.factor(season))
 }
 
+# Check if number only
+f_numbers_only <- function(x)
+  ! grepl("\\D", x)
+
+
+
 # getting stats from website
 d_regular_raw <-
   mclapply(urls_regular_stats, getStats, seasons) %>%
   bind_rows() %>%
-  dplyr:: rename(MPG = MP)
+  dplyr::rename(MPG = MP)
 
 d_advanced_raw <-
   mclapply(urls_advanced_stats, getStats,  seasons) %>% bind_rows()
+
 
 # merging regular and advanced stats
 d_season_combined <-
@@ -65,7 +80,18 @@ d_season_combined <-
             d_advanced_raw,
             by = c("Player", "Pos", "Age", "Team", "G",  "Season"))
 
-write.csv(d_season_combined, "season_combined.csv")
+# converting types
+d_season_combined$Pos <- as.factor(d_season_combined$Pos)
+d_season_combined$Team <- as.factor(d_season_combined$Team)
+d_season_combined$Season <- as.factor(d_season_combined$Season)
+d_season_combined$Player <- as.factor(d_season_combined$Player)
+
+d_season_combined <- d_season_combined %>%
+  mutate_if(is.character, as.numeric) 
+
+d_season_combined$Player <- as.character(d_season_combined$Player)
+
+# write.csv(d_season_combined, "season_combined.csv")
 
 # ----------------------------- Player All-Time stats -----------------------------------
 
@@ -79,12 +105,14 @@ write.csv(d_season_combined, "season_combined.csv")
 # ---------------------------------- League average----------------------------
 
 d_league_average <- d_season_combined %>%
-  select(-c(Player, Tm, Pos)) %>%
+  select(-c(Player, Team, Pos, Season)) %>%
   mutate_if(is.character, as.numeric) %>%
-  summarise_all(list(median = median, mean = mean), is.na =
-                  TRUE)
+  summarise_all(mean, na.rm = TRUE)
 
 # ----------------------------- residual around league average mean ---------------------
+#
+#  d_residuals <- d_season_combined %>%
+#    summarise_if()
 
-# d_residuals <- d_season_combined %>%
-#   revalue()
+
+# -------------------
